54ba7b709548e8b127a7401527895884ec2cfd0c
vlrusu <vrusu@fnal.gov>
2019-03-20 15:34:50 -0500

    0xFFFF on memlevel to agree with the FIFO size in serdes

--- utils.c
+++ utils.c
@@ -354,7 +354,7 @@ void read_data(int *delay_count, int *trigger_count, uint8_t hvcal)
 
 		if (memlevel < readout_wordsPerTrigger){
 			volatile uint32_t fifoinfo = *(registers_0_addr + rdcnt);
-			memlevel = fifoinfo & 0x7FFF;
+			memlevel = fifoinfo & 0xFFFF;
 			if (memlevel < readout_wordsPerTrigger){
 				(*delay_count)++;
 				if ((*delay_count) >= readout_maxDelay){
